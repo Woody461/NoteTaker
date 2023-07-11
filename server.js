64c4dfebbb88caa8e60bfef5765f407617fe5c29
Steve <stevemarti677@gmail.com>
2023-07-10 20:19:28 -0600

    added style

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 const express = require('express');
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.set('view engine', 'ejs');
-
-app.get('/notes', (req, res) => {
-  res.render('notes');
-});
-
+// API routes
 app.get('/api/notes', (req, res) => {
   fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
     if (err) {
@@ -61,10 +57,53 @@ app.post('/api/notes', (req, res) => {
   });
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+  
+    fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error reading notes data' });
+      }
+  
+      const notes = JSON.parse(data);
+  
+      // Find the index of the note with the matching id
+      const noteIndex = notes.findIndex((note) => note.id === noteId);
+  
+      // If the note is found, remove it from the notes array
+      if (noteIndex !== -1) {
+        notes.splice(noteIndex, 1);
+  
+        fs.writeFile(
+          path.join(__dirname, 'db.json'),
+          JSON.stringify(notes, null, 2),
+          'utf8',
+          (err) => {
+            if (err) {
+              console.error(err);
+              return res.status(500).json({ error: 'Error writing notes data' });
+            }
+  
+            res.sendStatus(204); // Send a success status with no content
+          }
+        );
+      } else {
+        res.status(404).json({ error: 'Note not found' });
+      }
+    });
+  });
+
+// HTML routes
+app.get('/notes', (req, res) => {
+  res.render('notes');
+});
+
 app.get('*', (req, res) => {
   res.render('index');
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
